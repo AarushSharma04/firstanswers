d7a4338bb3c367fdd1494ed177241bae9e034e52
ashdew12 <63419619+ashdew12@users.noreply.github.com>
2021-12-26 12:31:52 -0500

    base

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <HashRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <App />
   </HashRouter>,
   document.getElementById("root")
